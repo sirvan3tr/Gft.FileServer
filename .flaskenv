30b0b1caabd3ecb30069ab63d50cbd5a7b95735a
Sirvan Almasi <sirvan3tr@gmail.com>
2019-08-09 16:37:05 +0100

    updated names

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
-FLASK_APP=plotmaponline.py
+FLASK_APP=gftserver.py
 FLASK_ENV=development
--- app/routes.py
+++ app/routes.py
@@ -2,17 +2,21 @@ import os
 from app import app
 from flask import render_template, session, redirect, url_for, escape, request, send_from_directory
 from werkzeug.utils import secure_filename
+import json
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/')
 @app.route('/index')
 def index():
    return render_template('index.html')
+
 
 @app.route('/plot', methods=['GET', 'POST'])
 def plot():
@@ -34,22 +38,26 @@ def plot():
             return redirect(url_for('uploaded_file', filename=filename))
     return render_template('plot.html')
 
+
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory('uploads/',
                                filename)
 
+
 @app.route('/geocode')
 def geocode():
     return "hello"
 
+
 @app.route('/getfile/<file>', methods=['POST'])
-def plot(file):
+def getfile(file):
     return send_from_directory('uploads/',
                                filename)
 
+
 @app.route('/uploadfile', methods=['POST'])
-def plot():
+def uploadfile():
     # check if the post request has the file part
     if 'file' not in request.files:
         flash('No file part')
@@ -66,3 +74,9 @@ def plot():
         print(filename)
         return redirect(url_for('uploaded_file', filename=filename))
     return render_template('plot.html')
+
+
+@app.route('/master', methods=['POST'])
+def master():
+    req_data = request.get_json()
+    return json.dumps(req_data)
