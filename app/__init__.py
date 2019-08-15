d8f0201e36cac54e51b0a241eea34125790e27d5
sirvan3tr <sirvan3tr@gmail.com>
2019-08-15 15:41:20 +0100

    init and reqs

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
 
+db = MongoEngine(app)
 UPLOAD_FOLDER = 'app/uploads/'
 
 app = Flask(__name__)
 
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'project1',
+    'host': '192.168.1.35',
+    'port': 12345
+}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
 app.config['TESTING'] = True
 
-from app import routes
+from app import routesmongo
--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,10 @@ from flask import render_template, session, redirect, url_for, escape, request, 
 from werkzeug.utils import secure_filename
 import json
 
+import web3, json
+from web3 import Web3
+from eth_account.messages import defunct_hash_message
+
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
@@ -15,28 +19,7 @@ def allowed_file(filename):
 @app.route('/')
 @app.route('/index')
 def index():
-   return render_template('index.html')
-
-
-@app.route('/plot', methods=['GET', 'POST'])
-def plot():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print(filename)
-            return redirect(url_for('uploaded_file', filename=filename))
-    return render_template('plot.html')
+    return render_template('index.html')
 
 
 @app.route('/uploads/<filename>')
@@ -45,13 +28,27 @@ def uploaded_file(filename):
                                filename)
 
 
-@app.route('/geocode')
-def geocode():
-    return "hello"
+@app.route('/getfile', methods=['POST'])
+def getfile():
+    req_data = request.get_json()
+    '''
+    {
+        'type': 'gft.getfile'
+        'msg': {
+            deeID:
+            hashFile:
+            txID:
+            fileName:
+            timestamp:
+        }
+    }
+    '''
+    msgHash = defunct_hash_message(text=msg)
+    pubKey = w3.eth.account.recoverHash(msgHash, signature=sig)
 
+    # Search through the deeID contract for the above public key
+    
 
-@app.route('/getfile/<file>', methods=['POST'])
-def getfile(file):
     return send_from_directory('uploads/',
                                filename)
 
